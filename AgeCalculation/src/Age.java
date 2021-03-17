f5e5a46dc9c8b96365d8ec21d60af9a32c3e09e5
Ja'kee Pleasant <s1267652@monmouth.edu>
2021-03-17 19:29:56 -0400

    this is age calc

---
+++ AgeCalculation/src/Age.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+import javax.swing.JOptionPane;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDate today = LocalDate.now();
+		LocalDate bday = LocalDate.of(2001, 01, 18);
+		String inputwindow = JOptionPane.showInputDialog("Enter your brithday (MM/DD/YYYY): ");
+		
+		inputwindow = JOptionPane.showInputDialog("Enter today's date (MM/DD/YYYY): ");
+		
+		JOptionPane.showMessageDialog(null, "You are " + Period.between(bday, today).getYears() + " years old");
+		
+	}
+
+}
